8018446f4f839ce9c8fcd151bb2c4d5d361efe9f
231250157-Ying Jiaqi <1848877638@qq.com>
2024-11-28 23:34:34 +0800

    注册和登录

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const users = [];
+let current_user = -1
+let author = ''
+const cycles = []
+const image_begin = " ";
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -16,3 +21,32 @@ app.get('/about', (req, res) => {
 
 app.use(express.static('public'));
 //静态文件目录，访问http://localhost:3000/your-file-name即可访问public目录下的文件
+
+app.post('/api/login',(req,res)=>{
+    const { username, password } = req.body;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username === username && users[i].password === password) {
+            current_user = i;
+            author = users[current_user].username;
+            res.render('homepage');
+        }
+    }
+    res.status(401).send('"Invalid email or password');
+})
+
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    // 检查用户是否已经存在
+    if (users.some(user => user.username === username)) {
+        res.status(400).send('Username already exists');
+    } else {
+        const comments = []
+        const user = {
+            "comments":comments,
+            "username": username,
+            "password": password,
+            "image": image_begin,
+        }
+        users.push(user);
+    }
+});
